54f4f74f85ae6b24d1f74e8e6c0fd75a518dc44b
brakesh <bandarupallirakesh@gmail.com>
2022-01-19 22:58:15 +0530

    Shared library updated

--- terraform-mutable/data.tf
+++ terraform-mutable/data.tf
@@ -21,3 +21,10 @@ data "terraform_remote_state" "alb" {
     region = "us-east-1"
   }
 }
+data "aws_secretsmanager_secret" "common" {
+  name = "common/ssh"
+}
+
+data "aws_secretsmanager_secret_version" "secrets" {
+  secret_id = data.aws_secretsmanager_secret.common.id
+}
