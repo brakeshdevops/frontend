58e7081eca091f0cf315b9141139bc4e0f5a61a2
brakesh <bandarupallirakesh@gmail.com>
2022-01-19 15:10:06 +0530

    Shared library updated

--- terraform-mutable/ec2.tf
+++ terraform-mutable/ec2.tf
@@ -8,6 +8,7 @@ count=var.INSTANCE_COUNT
   }
   subnet_id = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS[count.index]
   wait_for_fulfillment = true
+  vpc_security_group_ids = aws_security_group.sg.id
 }
 resource "aws_ec2_tag" "spot_instance" {
   count=length(aws_spot_instance_request.ec2-spot)
@@ -26,4 +27,13 @@ resource "aws_lb_target_group_attachment" "tg-attach" {
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = aws_spot_instance_request.ec2-spot.*.spot_instance_id[count.index]
   port=80
+}
+resource "aws_lb_listener" "lb-listener" {
+  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_LB_ARN
+  port="80"
+  protocol="HTTP"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
 }
